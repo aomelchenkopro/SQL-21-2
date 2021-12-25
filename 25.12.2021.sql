a60a39ff08a3f0f078b4bc800155cc960799a894
mirosh9719 <mirosh9719@gmail.com>
2021-12-25 15:04:58 +0200

    Update 25.12.2021.sql

--- 25.12.2021.sql
+++ 25.12.2021.sql
@@ -124,8 +124,14 @@ select t2.BusinessEntityID,
  where t1.JobTitle = N'Design Engineer'
 ;															
 																		
-																		
-																		
+-- Мирошниченко Анастасия
+select t1.BusinessEntityID,
+       concat_ws(N' ', ltrim(t2.LastName), ltrim(t2.FirstName), ltrim(t2.MiddleName)) as [FULL_NAME]
+  from [HumanResources].[Employee] as t1
+ inner join [Person].[Person]      as t2 on t2.BusinessEntityID = t1.BusinessEntityID
+ where t1.JobTitle = N'Production Supervisor - WC60'
+;
+																
 																		
 																														
 																		
