9d334a45f60d975b62e32eefa0b83619ca6abf08
Анна Гошкодер <85807541+AnnH1994@users.noreply.github.com>
2021-12-25 15:09:07 +0200

    Update 25.12.2021.sql

--- 25.12.2021.sql
+++ 25.12.2021.sql
@@ -126,7 +126,16 @@ select t2.BusinessEntityID,
 																		
 																		
 																		
-																		
+--Молчанов 25.12.21
+select t1.BusinessEntityID,
+       concat_ws (N' ', t2.FirstName,  t2.MiddleName, t2.LastName)
+   from [HumanResources].[Employee] as t1
+ inner join [Person].[Person] as t2 on t2.BusinessEntityID=t1.BusinessEntityID
+   where t1.JobTitle=N'Production Supervisor - WC60';
+
+
+
+
 																														
 																		
 																		
