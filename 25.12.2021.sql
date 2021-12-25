819478e3113029ddc3e8ce8e5856a797ea0143e3
tburakova <tatyana.burakova.97@gmail.com>
2021-12-25 15:19:25 +0200

    Update 25.12.2021.sql

--- 25.12.2021.sql
+++ 25.12.2021.sql
@@ -123,16 +123,13 @@ select t2.BusinessEntityID,
  inner join [Person].[Person]      as t2 on t2.BusinessEntityID = t1.BusinessEntityID
  where t1.JobTitle = N'Design Engineer'
 ;															
-																		
-																		
-																		
-																		
-																														
-																		
-																		
-																		
-																		
-																		
+																																				
+-- Пасека Дарья 																																																																					
+select t2.BusinessEntityID,
+       concat_ws(N' ', ltrim(t2.LastName), ltrim(t2.FirstName), ltrim(t2.MiddleName)) as [full_name]
+  from [HumanResources].[Employee] as t1
+ inner join [Person].[Person]      as t2	on t2.BusinessEntityID = t1.BusinessEntityID														
+where t1.JobTitle = N'Production Supervisor - WC60'																
 																		
 																		
 																		
