aed6f247c2f52a30b93c7f29a103f18a73e98805
Alexander <aomelchenko.pro@outlook.com>
2021-12-26 16:14:20 +0200

    Delete files

--- Пасека2.sql
+++
@@ -1,7 +0,0 @@
-																																				
--- Пасека Дарья 																																																																					
-select t2.BusinessEntityID,
-       concat_ws(N' ', ltrim(t2.LastName), ltrim(t2.FirstName), ltrim(t2.MiddleName)) as [full_name]
-  from [HumanResources].[Employee] as t1
- inner join [Person].[Person]      as t2	on t2.BusinessEntityID = t1.BusinessEntityID														
-where t1.JobTitle = N'Production Supervisor - WC10'	
